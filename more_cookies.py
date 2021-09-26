9d091b72b7995c735ee9154493dfe18e935acfb3
BLAINE777 <74994823+BORNY444@users.noreply.github.com>
2021-09-26 17:37:59 -0400

    Update more_cookies.py

--- more_cookies.py
+++ more_cookies.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 
 import requests
 from base64 import b64decode, b64encode
@@ -13,14 +14,14 @@ def bit_flip(pos, bit, data):
     raw = bytes(list1)
     return b64encode(b64encode(raw)).decode()
 
-cookie = "citkRWxXSVVkaForamVZWFVjeHZwdG00ZG5ockJJU3J1NEp5aXh6VEptOUxqKzY3dVJmcXJ0VkZmN0ZJVm16Mk1nNS9aMnBOSUhBaDZveEFUcGQ0T3diRGllNVVDeWEyOFk1cjJCS0g4R0RySWdzdGswYnhhNkN1M1NIeWJSb3c="
+cookie = "citkRWxXSVVkaForamVZWFVjeHZwdG00ZG5ockJJU3J1NEp5aXh6VEptOUxqKzY3dVJmcXJ0VkZmN0ZJVm16Mk1nNS9aMnBOSUhBaDZveEFUcGQ0T3diRGllNVVDeWEyOFk1cjJCS0g4R0RySWdzdGswYnhhNkN1M1NIeWJSb3c=" # change it with urs
 
 for position_idx in tqdm(range(10), desc="Bruteforcing Position"):
     # The 96 really should be 128 to test every bit, but 96 worked for me.
     for bit_idx in tqdm(range(96), desc="Bruteforcing Bit"):
         auth_cookie = bit_flip(position_idx, bit_idx, cookie)
         cookies = {'auth_name': auth_cookie}
-        r = requests.get('http://mercury.picoctf.net:53700/', cookies=cookies)
+        r = requests.get('http://mercury.picoctf.net:53700/', cookies=cookies)  #change it with urs
         if "picoCTF{" in r.text:
             # The flag is between `<code>` and `</code>`
             print("Flag: " + r.text.split("<code>")[1].split("</code>")[0])
